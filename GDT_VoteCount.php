c1e6686176150240495bbc513882112e9f98b884
gizmore <gizmore@wechall.net>
2022-08-17 19:22:19 +0200

    '7.0.1-r1516 unit tests'

--- GDT_VoteCount.php
+++ GDT_VoteCount.php
@@ -1,30 +1,43 @@
 <?php
 namespace GDO\Votes;
 
+use GDO\Core\GDO;
 use GDO\Core\GDT_Template;
 use GDO\Core\GDT_UInt;
+use GDO\Core\WithGDO;
 
+/**
+ * Display a votecount.
+ * 
+ * @author gizmore
+ * @version 7.0.1
+ * @since 6.5.0
+ */
 class GDT_VoteCount extends GDT_UInt
 {
-	public $writable = false;
-	public $editable = false;
+	use WithGDO;
 	
-	public function defaultLabel() : self { return $this->label('votes'); }
+	public function defaultLabel() : self
+	{
+		return $this->label('votes');
+	}
 
 	protected function __construct()
 	{
 		parent::__construct();
 		$this->initial = "0";
+		$this->writeable = false;
 	}
 	
-	public function getVoteObject()
+	public function getVoteObject() : GDO
 	{
 		return $this->gdo;
 	}
 	
 	public function renderHTML() : string
 	{
-		return GDT_Template::php('Votes', 'cell/vote_count.php', ['field'=>$this]);
+		return GDT_Template::php('Votes', 'votecount_html.php', [
+			'field' => $this]);
 	}
 	
 }
--- GDT_VoteRating.php
+++ GDT_VoteRating.php
@@ -4,21 +4,25 @@ namespace GDO\Votes;
 use GDO\Core\GDO;
 use GDO\Core\GDT_Template;
 use GDO\Core\GDT_Decimal;
+use GDO\Core\WithGDO;
 
 final class GDT_VoteRating extends GDT_Decimal
 {
-    public $orderDefaultAsc = false;
-    
+	use WithGDO;
+	
     public function defaultLabel() : self { return $this->label('rating'); }
+
+    public function isDefaultAsc() : bool { return false; }
 	
 	/**
 	 * @return GDO|WithVotes
 	 */
-	public function getVoteObject() { return $this->gdo; }
+	public function getVoteObject() : GDO { return $this->gdo; }
 	
 	protected function __construct()
 	{
-	    $this->writable = false;
+		parent::__construct();
+	    $this->writeable = false;
 	    $this->editable = false;
 		$this->digits(2, 2);
 		$this->notNull();
