6688af5f750186a67a1a3ba5290f532d0660b20a
gizmore <gizmore@wechall.net>
2022-09-06 23:19:43 +0200

    '7.0.1-r1564 fixes'

--- tpl/votecount_html.php
+++ tpl/votecount_html.php
@@ -4,9 +4,10 @@ use GDO\UI\GDT_Badge;
 use GDO\Votes\GDT_VoteCount;
 /** @var $field GDT_VoteCount **/
 $gdo = $field->getVoteObject(); ?>
-<span class="<?=$field->name;?>-vote-count-<?= $gdo->getID(); ?>">
+<div class="gdt-votecount"
+ id="<?=$field->name?>-vote-count-<?=$gdo->getID()?>">
 <?php
 $value = t('vote_count', [$gdo->getVoteCount()]);
 echo GDT_Badge::make()->addClass('vote-count')->var($value)->renderHTML();
 ?>
-</span>
+</div>
